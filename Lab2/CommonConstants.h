ca034ab9d43749ab5c43a6193d9fc03553fdb2cf
fizmatbfu <fizmatbfu@gmail.com>
2020-04-15 12:02:12 +0200

    add missing header

---
+++ Lab2/CommonConstants.h
@@ -0,0 +1,8 @@
+#ifndef  __OPENSSL_COMMONCONSTANTS__
+#define  __OPENSSL_COMMONCONSTANTS__
+
+const size_t kNumOfMegabytesForTest = 20;
+
+constexpr size_t kSize = kNumOfMegabytesForTest * 1024 * 1024 * 8 / sizeof(unsigned char);
+
+#endif // ! __OPENSSL_COMMONCONSTANTS__
